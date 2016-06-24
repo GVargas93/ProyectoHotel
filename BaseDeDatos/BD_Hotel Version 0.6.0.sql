0bdeea6a239ed04e3f62865e5c7485dc9408266f
jose clavijo <ja.clavijo95@gmail.com>
2016-06-24 11:22:40 -0700

    elimine la tabla  estado  y la columna de la reserva  que es estado

---
+++ BaseDeDatos/BD_Hotel Version 0.6.0.sql
@@ -0,0 +1,18 @@
+-- =================================================
+-- Autor:			Gerardo, Jose, Alvaro
+-- Fecha:			19/04/2016
+-- Descripción:		Hotel-Software
+-- Versión:			0.6.0
+-- =================================================
+
+------------------------------------- Soy Una Barra Separadora  :) ----------------------------------------
+
+USE DB_Hotel
+GO
+
+------------------------------------- Soy Una Barra Separadora :) ----------------------------------------
+
+DROP TABLE[dbo].[Tbl_Estado]
+
+
+ALTER TABLE [dbo].[Tbl_Reserva] DROP CONSTRAINT  FK_Tbl_ReservaEstado 
